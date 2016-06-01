078310ad37dba7205e54250967a75f69275c0bc6
Jeremy Evans <jeremy.n.evans@gmail.com>
2016-06-01 21:29:43 +0100

    Old script out, Two layers

--- public/demo/script_old.js
+++ public/demo/script_old.js
@@ -23,21 +23,13 @@ $.doctop({
 });
 
 window.setTimeout(function() {
-  // if (waitingForDoctop) {
+  if (waitingForDoctop) {
     waitingForDoctop = false;
     console.log('Doctop not loaded - using backup data...');
     cards = {"0":{"id":"0","topic":"Heathrow Drone","title":"Drone hits Heathrow plane","body":"<a href=\"#1\">A British Airways flight from Geneva</a> is <a href=\"#2\">believed to have hit a drone</a> before <a href=\"#8\">landing safely at Heathrow</a> airport, raising <a href=\"#3\">concerns over aviation safety</a>.","headline":"true","coverImage":"https://pixabay.com/static/uploads/photo/2015/12/29/13/13/drone-1112752_960_720.jpg","draftOrAuthor":"yukiko"},"1":{"id":"1","topic":"Heathrow Drone","title":"A British Airways flight","body":"The flight BA727 from Geneva to Heathrow was carrying 132 passengers and 5 crew. <a href=\"#5\">The Airbus A320</a> plane <a href=\"#7\">was cleared to take off the next flight after being examined</a>.","draftOrAuthor":"yukiko"},"2":{"id":"2","topic":"Heathrow Drone","title":"Believed to have hit a drone","body":"The pilot reported an object that is believed to be a drone struck the front of <a href=\"#1\">the flight</a>, and it would be the first incident of its kind in the UK if confirmed. \n        <a href=\"#6\">The investigation is underway</a>.","draftOrAuthor":"yukiko"},"3":{"id":"3","topic":"Drone and aviation safety","title":"Concerns over aviation safety and drone","body":"<a href=\"#10\">Pilots have called for an investigation</a> into the likely effects of <a href=\"#9\">a drone strike on an aircraft</a> last month, following <a href=\"#4\">a report on their near-misses</a>.","draftOrAuthor":"yukiko"},"4":{"id":"4","topic":"Drone and aviation safety","title":"Report by the UK Airpox Board","body":"There were 23 near-misses between drones and aircraft in the 6 months between April and October last year.","draftOrAuthor":"yukiko"},"5":{"id":"5","topic":"Heathrow Drone","title":"Airbus A320 family","body":"The A320 manufactured by Airbus typically seats 150 passengers in a two-class cabin, and is commonly used by commercial flights.","draftOrAuthor":"yukiko"},"6":{"id":"6","topic":"Heathrow Drone","title":"Investigation on ‘drone’ claim","body":"Police says no arrests have been made. \n        The British Airline will give the police “every assistance with their investigation”.","draftOrAuthor":"yukiko"},"7":{"id":"7","topic":"Heathrow Drone","title":"Quote","body":"A British Airways spokesperson said: \n        “Our aircraft landed safely, was fully examined by our engineers and it was cleared to operate its next flight”.","draftOrAuthor":"yukiko"},"8":{"id":"8","topic":"Heathrow Drone","title":"Landing safely at Heathrow airport","body":"Despite a hit by an object, believed to be a drone, the flight with 132 passengers and 5 crew <a href=\"#7\">landed safely without damage to the aircraft</a>.","draftOrAuthor":"yukiko"},"9":{"id":"9","topic":"Drone and aviation safety","title":"Drone strike on aircraft","body":"<a href=\"#11\">People who fly drones</a> close to planes could be convicted of endangering aviation safety, which has a maximum prison sentence of five years, according to the Civil Aviation Authority.","draftOrAuthor":"yukiko"},"10":{"id":"10","topic":"Drone and aviation safety","title":"Pilots have called for an investigation","body":"The British Airline Pilots Association wants the Department for Transport and the Civil Aviation Authority to investigate into the effects of <a href=\"#9\">a drone strike on an aircraft</a>.","draftOrAuthor":"yukiko"},"11":{"id":"11","topic":"Drone and aviation safety","title":"People flying drones","body":"<a href=\"#12\">The Civil Aviation Authority is focusing on educating people</a> who use drones, fearing that many of them are not familiar with the legal issues.","draftOrAuthor":"yukiko"},"12":{"id":"12","topic":"Drone and aviation safety","title":"CAA and “dronecode”","body":"The Civil Aviation Authority launched Dronecode to simplify the rules over drones.","draftOrAuthor":"yukiko"}};
-
-    var card = cards[0];
-    var tempTemplate = cardTemplate(card.id, card.title, card.body, card.coverImage, card.topic, card.headline);
-    cardDOM = $(tempTemplate).appendTo('.cards');
-    console.log(cardDOM);
-    window.setTimeout(function() {
-      $('.card').removeClass('opening');
-    }, 300);
-    // openLayer([1,2,3],0);
-  // }
-}, 300);
+    openCard(0, null);
+  }
+}, 3000);
 
 var cardCarouselTemplate = function (id, title, body, image, topic, showHeaderImage) {
   if (!image) {
@@ -116,17 +108,11 @@ var getPosition = function(cardDOM) {
 
 var openLayer = function(keys, index) {
   var template = '';
-  $.each(keys, function(i, key) {
+  $.each(keys, function(key, i) {
     var card = cards[key];
     template = template + cardTemplate(card.id, card.title, card.body, card.coverImage, card.topic, card.headline);
   });
   template = '<div class="card-carousel">' + template + '</div>';
-
-  // template = '<div class="card-carousel">'
-  //     + '<div><div style="background:red; height: 100px; width: 200px"></div></div>'
-  //     + '<div><div style="background:red; height: 120px; width: 200px"></div></div>'
-  //     + '<div><div style="background:red; height: 130px; width: 200px"></div></div>'
-  //     + '</div>';
 
   cardDOM = $(template).appendTo('.cards');
 
@@ -143,24 +129,145 @@ var openLayer = function(keys, index) {
   $('.card').removeClass('opening');
 }
 
-var openCard = function(keys) {
-  console.log(keys);
+
+
+
+
+
+
+
+// Manipulate Card DOM
+var focusCardDOM = function(position) {
+  // console.log('focusCardDOM', position);
+  var cardDOM = $('.cards .card:not(.removed):eq(' + position + ')');
+  var previousPosition = position==0 ? position : position-1;
+  var previousCardDOM = $('.cards .card:not(.removed):eq(' + previousPosition + ')');
+  $('.card').addClass('faded').removeClass('opening');
+  cardDOM.removeClass('faded');
+  $('.card .card-visible').each(function() {
+    var newZIndex = parseInt($(this).css('z-index')) - 1;
+    // $(this).css('z-index',newZIndex);
+  });
+  cardDOM.find('.card-visible').css({ 'width': cardDOM.find('.card-spacer').css('width') }); // Not sure why but this is still necessary! For when cards first load.
+  console.log(cardDOM.offset().top);
+  console.log(cardDOM.find('.card-visible').height());
+  console.log(document.body.clientHeight);
+  var scrollPos = cardDOM.offset().top + cardDOM.find('.card-visible').height() - document.body.clientHeight + 20;
+  console.log(scrollPos);
+  console.log('---------');
+ //previousCardDOM.offset().top - 80
+  $('html,body').stop().animate({scrollTop: scrollPos},'slow');
+  setZValues();
+  reDrawIfOutOfSync();
+}
+var addCardDOM = function(list, cardKey, position) {
+  var card = cards[cardKey];
+  var template = cardCarouselTemplate(card.id, card.title, card.body, card.coverImage, card.topic, card.headline);
+  var cardDOM;
+  if (position != -1 && $('.card:not(.removed)').length) { //Doesn't yet handle multiple lists
+    var openerCard = $('.cards .card:not(.removed):eq(' + (position-1) + ')');
+    cardDOM = $(template).insertAfter(openerCard);
+  } else {
+    cardDOM = $(template).appendTo('.cards');
+  }
+  window.setTimeout(function() {
+    cardDOM.find('.card-spacer').css('height', cardDOM.find('.card-visible').height());
+    // focusCard(0, position);
+  }, 100);
+  reDrawIfOutOfSync();
+}
+var removeCardDOM = function(list, position) {
+  $('.cards .card:not(.removed):eq(' + (position) + ')').addClass('removed').fadeOut(500, function() { $(this).remove(); }); // Needs to change height gradually
+  reDrawIfOutOfSync();
+}
+var moveCardDOM = function(list, moveFrom, moveTo) { // This should soon have a move animation instead of just removing then adding
+  var key = getKeyFromCardDOM(list, moveFrom);
+  addCardDOM(0, key, moveTo);
+  var newMoveFrom = moveTo < moveFrom ? moveFrom+1 : moveFrom; //Reflects the fact that moveTo has been inserted and pushed subsquent elements forward
+  removeCardDOM(0, newMoveFrom); // moveFrom has already been adjusted and passed here from moveCard function
+  reDrawIfOutOfSync();
+}
+var setZValues = function() { // Doesn't yet handle multiple lists
+  $('.card:not(.removed)').each(function(i, card) {
+    var zValue = 1000 - Math.abs(i - focusPosition[0]);
+    var zScale = 1 - Math.pow(0.6, Math.abs(i - focusPosition[0]));
+    // $(card).find('.card-visible').css({'z-index': zValue, 'transform': 'scale(' + (1 - zScale/4) + ',' + (1 - zScale/4) + ')'});
+    // $(card).find('.card-grey').css('background', 'rgba(221,221,221,' + zScale + ')');
+  });
 }
 
+// Top-level commands (data manipulation which relies on Specific Card Functions)
+var openCard = function(cardToOpen, positionFrom) {
+  if (positionFrom == null || positionFrom < 0) {
+    positionFrom = cardLists[0].length - 1;
+  }
+  var existingCardPos = cardLists[0].indexOf(cardToOpen);
+  if (existingCardPos == -1) {
+    addCard(0, cardToOpen, positionFrom + 1);
+  } else {
+    moveCard(0, existingCardPos, positionFrom + 1);
+  }
+    focusCard(0, positionFrom + 1);
+}
+var closeCard = function(list, cardPos) {
+  removeCard(list, cardPos);
+  if (focusPosition[list] == cardPos) {
+    if (cardPos == cardLists[list].length) {
+      focusCard(list, cardPos-1);
+    } else {
+      focusCard(list, cardPos);
+    }
+  }
+}
 
+// Specific Card Functions that trigger, and correspond to, DOM Functions
+var focusCard = function(list, position) {
+  if (position >= 0 && position < cardLists[list].length) {
+    focusPosition[list] = position;
+    window.setTimeout(function() {
+      focusCardDOM(position);
+      window.setTimeout(function() {
+        $.each(cardLists[list], function(i, card) {
+          if (i > position) {
+            console.log('i: ' + i + ', position: ' + position);
+            removeCard(list, i);
+          }
+        });
+      }, 100);
+    }, 10);
+  }
+}
+var addCard = function(list, cardKeyToOpen, position) {
+  insertCard(list, cardKeyToOpen, position);
+  addCardDOM(0, cardKeyToOpen, position);
+}
+var removeCard = function(list, pos) {
+  deleteCard(list, pos);
+  removeCardDOM(list, pos);
+}
+var moveCard = function(list, moveFrom, moveTo) {
+  var key = cardLists[list][moveFrom];
+  insertCard(list, key, moveTo);
+  var newMoveFrom = moveTo < moveFrom ? moveFrom+1 : moveFrom; //Reflects the fact that moveTo has been inserted and pushed subsquent elements forward
+  deleteCard(list, newMoveFrom);
+  moveCardDOM(0, moveFrom, moveTo);
+}
 
+// General Card Functions (used by Specific Card Functions)
+var insertCard = function(list, cardKeyToOpen, position) {
+  cardLists[list].splice(position, 0, cardKeyToOpen);
+}
+var deleteCard = function(list, pos) {
+  cardLists[list].splice(pos, 1);
+}
 
 
 
 //UI Interaction
 $(".cards").on("click", "a", function(){
-  var cardToOpenKey = $(this).attr('href').substring(1);
-  var allKeys = [];
-  $.each($(this).closest('.body-content').find('a'), function(i, link) {
-    console.log(link);
-    allKeys.push($(link).attr('href').substring(1));
-  });
-  openLayer(allKeys, 0);
+  var cardToOpen = $(this).attr('href').substring(1); //Key of card to open
+  var position = getPosition($(this).parents('.card')[0]);
+  openCard(cardToOpen, position);
 });
 $(".cards").on("click", "i.close", function(){
   var card = $(this).closest('.card');
@@ -246,4 +353,38 @@ $( window ).resize(function() {
   $('.card').each(function() {
     $(this).find('.card-visible').css({ 'width': $(this).find('.card-spacer').css('width') });
   });
+});
+
+
+
+
+
+
+
+var hammertime = new Hammer(document, []);
+hammertime.get('swipe').set({ direction: Hammer.DIRECTION_VERTICAL });
+hammertime.get('pan').set({ direction: Hammer.DIRECTION_VERTICAL });
+hammertime.on('panend', function(ev) {
+  console.log('panend');
+	console.log(ev);
+  focusCard(0, focusPosition[0]);
+});
+hammertime.on('swipeup', function(ev) {
+  console.log('swipeup');
+	console.log(ev);
+  focusCard(0, focusPosition[0]+1);
+});
+hammertime.on('swipedown', function(ev) {
+  console.log('swipedown');
+	console.log(ev);
+  focusCard(0, focusPosition[0]-1);
+});
+
+
+
+
+$(document).ready(function(){
+  // $('.card-carousel').slick({
+  //   // setting-name: setting-value
+  // });
 });
