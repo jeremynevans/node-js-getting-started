a2fbd53619e7a9b6570c009cf58949d5bd31394e
Jeremy Evans <jeremy.n.evans@gmail.com>
2016-10-05 11:11:23 +0100

    Webhook for Facebook Chatbot

--- index.js
+++ index.js
@@ -33,6 +33,22 @@ app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
+
+
+//Facebook Chatbot
+
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === 'explain_the_news') {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);
+  }
+});
+
+
 // sass.render({
 //   file: 'views/scss.main.scss',
 //   [, options..]
